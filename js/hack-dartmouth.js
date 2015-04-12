5c327d9d8503a6be3f78b652da0b894ddbae1a9d
atoregozh <toregozh@gmail.com>
2015-04-12 09:13:47 -0400

    got flags to work

--- js/hack-dartmouth.js
+++ js/hack-dartmouth.js
@@ -1,1 +1,29 @@
 /* Insert JavaScript here */
+$(document).ready(function(){
+
+  $("#person-account").click(function(){
+    console.log("person selected");
+    $("#account-type").html("Person <span class='caret'></span>" );
+  });
+
+  $("#org-account").click(function(){
+    console.log("org selected");
+    $("#account-type").html("Organization <span class='caret'></span>" );
+  });
+
+  $("#phone").intlTelInput({
+    allowExtensions: false,
+    autoFormat: false,
+    autoHideDialCode: false,
+    autoPlaceholder: true,
+    defaultCountry: "ng",
+    ipinfoToken: "yolo",
+    nationalMode: false,
+    numberType: "MOBILE",
+    onlyCountries: ['ng', 'us', 'gb'],
+    preferredCountries: [],
+    // utilsScript: "libphonenumber/build/utils.js" //commented out to stop complain "XMLHttpRequest cannot load...Cross origin requests are only supported for protocol schemes: http, data,"
+  });
+
+
+});
