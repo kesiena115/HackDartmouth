5ff581e0157089bd0ffdd4c4c1cb4a56a0eda0c2
atoregozh <toregozh@gmail.com>
2015-04-18 16:18:18 -0400

    kesiena copies

---
+++ js/kesiena.js
@@ -0,0 +1,29 @@
+/* Insert JavaScript here */
+$(document).ready(function(){
+
+  $("#person-account").click(function(){
+    console.log("person selected");
+    $("#account-type").html("Person <span class='caret'></span>" );
+  });
+
+  $("#org-account").click(function(){
+    console.log("org selected");
+    $("#account-type").html("Organization <span class='caret'></span>" );
+  });
+
+  $("#phone").intlTelInput({
+    allowExtensions: false,
+    autoFormat: true,
+    autoHideDialCode: false,
+    autoPlaceholder: true,
+    defaultCountry: "ng",
+    ipinfoToken: "yolo",
+    nationalMode: true,
+    numberType: "MOBILE",
+    onlyCountries: ['ng', 'us', 'gb'],
+    preferredCountries: [],
+    utilsScript: "js/intlTelIutils.js" 
+  });
+
+
+});
