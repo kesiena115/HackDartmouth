1e6c44d3dbf177959fb895acf1421abf167a02d7
atoregozh <toregozh@gmail.com>
2015-04-19 02:06:46 -0400

    minor styling

--- js/hack-dartmouth.js
+++ js/hack-dartmouth.js
@@ -2,13 +2,52 @@
 $(document).ready(function(){
 
   $("#person-account").click(function(){
-    console.log("person selected");
     $("#account-type").html("Person <span class='caret'></span>" );
   });
 
   $("#org-account").click(function(){
-    console.log("org selected");
     $("#account-type").html("Organization <span class='caret'></span>" );
+  });
+
+  $("#dr-title").click(function(){
+    $("#title-select").html("Dr <span class='caret'></span>" );
+  });
+
+  $("#mr-title").click(function(){
+    $("#title-select").html("Mr <span class='caret'></span>" );
+  });
+
+  $("#mrs-title").click(function(){
+    $("#title-select").html("Mrs <span class='caret'></span>" );
+  });
+
+  $("#miss-title").click(function(){
+    console.log("miss selected");
+    $("#title-select").html("Miss <span class='caret'></span>" );
+  });
+
+  $("#business-org").click(function(){
+    $("#industry-select").html("Business <span class='caret'></span>" );
+  });
+
+  $("#edu-org").click(function(){
+    $("#industry-select").html("Education <span class='caret'></span>" );
+  });
+
+  $("#govern-org").click(function(){
+    $("#industry-select").html("Government Administration <span class='caret'></span>" );
+  });
+
+  $("#religious-org").click(function(){
+    $("#industry-select").html("Religious Institution <span class='caret'></span>" );
+  });
+
+  $("#social-org").click(function(){
+    $("#industry-select").html("Social Group <span class='caret'></span>" );
+  });
+
+  $("#other-org").click(function(){
+    $("#industry-select").html("Other <span class='caret'></span>" );
   });
 
   $("#phone").intlTelInput({
