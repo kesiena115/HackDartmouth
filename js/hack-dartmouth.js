b8767c03e7f915a443928098583b5f27f0c7646c
atoregozh <toregozh@gmail.com>
2015-04-21 16:30:45 -0400

    font awesome added

--- js/hack-dartmouth.js
+++ js/hack-dartmouth.js
@@ -2,52 +2,52 @@
 $(document).ready(function(){
 
   $("#person-account").click(function(){
-    $("#account-type").html("Person <span class='caret'></span>" );
+    $("#account-type").html("Person <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#org-account").click(function(){
-    $("#account-type").html("Organization <span class='caret'></span>" );
+    $("#account-type").html("Organization <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#dr-title").click(function(){
-    $("#title-select").html("Dr <span class='caret'></span>" );
+    $("#title-select").html("Dr <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#mr-title").click(function(){
-    $("#title-select").html("Mr <span class='caret'></span>" );
+    $("#title-select").html("Mr <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#mrs-title").click(function(){
-    $("#title-select").html("Mrs <span class='caret'></span>" );
+    $("#title-select").html("Mrs <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#miss-title").click(function(){
     console.log("miss selected");
-    $("#title-select").html("Miss <span class='caret'></span>" );
+    $("#title-select").html("Miss <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#business-org").click(function(){
-    $("#industry-select").html("Business <span class='caret'></span>" );
+    $("#industry-select").html("Business <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#edu-org").click(function(){
-    $("#industry-select").html("Education <span class='caret'></span>" );
+    $("#industry-select").html("Education <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#govern-org").click(function(){
-    $("#industry-select").html("Government Administration <span class='caret'></span>" );
+    $("#industry-select").html("Government Administration <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#religious-org").click(function(){
-    $("#industry-select").html("Religious Institution <span class='caret'></span>" );
+    $("#industry-select").html("Religious Institution <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#social-org").click(function(){
-    $("#industry-select").html("Social Group <span class='caret'></span>" );
+    $("#industry-select").html("Social Group <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#other-org").click(function(){
-    $("#industry-select").html("Other <span class='caret'></span>" );
+    $("#industry-select").html("Other <i class='fa fa-caret-down pull-right'></i>" );
   });
 
   $("#phone").intlTelInput({
